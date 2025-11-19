c4f4d8883f16de7e6c04dbc6da0d46190e668d73
benjamin <benjamin.edgard@gmail.com>
2025-11-19 01:58:33 +0100

    header

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import "../styles/Header.css";
+import banner from "../images/banner.jpg";
+
+function Header() {
+  return (
+    <div className="header-banner">
+      <h1 className="header-title">Ménage régulier ou ponctuel</h1>
+      <a href="#" className="header-button">
+        Réserver mon ménage
+      </a>
+      <img src={banner} alt="bannière qui montre une femmme au ménage" />
+    </div>
+  );
+}
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./components/Navbar";
 import "./index.css";
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Navbar />
+    <Header />
   </StrictMode>
 );

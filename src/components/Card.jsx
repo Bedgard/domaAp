9c4f377f453487ba52f4afb381a66f8facbed964
benjamin <benjamin.edgard@gmail.com>
2026-01-02 19:33:30 +0100

    page-accueil-fin

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,14 +2,12 @@ import "../styles/Card.css";
 
 function Card({ title, description, color, image }) {
   return (
-    <div className="card-container">
-      <div className={`card-container-img ${color}`}>
-        <h1>{title}</h1>
-        <img src={image} alt={description} />
-        <a href="#" className="card-button">
-          Réservez maintenant
-        </a>
-      </div>
+    <div className={`card-container-img ${color}`}>
+      <h1>{title}</h1>
+      <img src={image} alt={description} />
+      <a href="#" className="card-button">
+        Réservez maintenant
+      </a>
     </div>
   );
 }

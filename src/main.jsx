0c78f22172667acaef8906fd0e0ac5538ab1d70f
benjamin <benjamin.edgard@gmail.com>
2026-01-02 19:12:42 +0100

    page-accueil-termine

--- src/main.jsx
+++ src/main.jsx
@@ -9,21 +9,25 @@ import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import About from "./components/About";
 import Card from "./components/Card";
+import { nanoid } from "nanoid";
 
 const cardsDatas = [
   {
+    id: nanoid(),
     title: "repassage",
     description: "une femme de ménage en train de repasser",
     color: "blue-opacity",
     image: repassage,
   },
   {
+    id: nanoid(),
     title: "menage complet",
     description: "un homme qui range tout l'intérieur",
     color: "green-opacity",
     image: menageComplet,
   },
   {
+    id: nanoid(),
     title: "menage rapide",
     description: "une femme de ménage qui nettoie",
     color: "red-opacity",
@@ -42,9 +46,9 @@ createRoot(document.getElementById("root")).render(
         <span className="title-grid">WeCasa</span> est là pour vous servir !
       </h2>
       <div className="grid-container">
-        {cardsDatas.map((card, index) => (
+        {cardsDatas.map((card) => (
           <Card
-            key={index}
+            key={card.id}
             title={card.title}
             description={card.description}
             color={card.color}

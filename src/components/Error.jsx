77229079b1e5e8e2286777b1350b9fccc9cceadd
benjamin <benjamin.edgard@gmail.com>
2026-05-15 18:22:40 +0200

    page-error

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,4 +1,10 @@
+import "../styles/Error.css";
+
 function Error() {
-  return <div>Error</div>;
+  return (
+    <div className="Error">
+      <h2>Erreur 404</h2>
+    </div>
+  );
 }
 export default Error;
